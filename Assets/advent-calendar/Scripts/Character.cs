c1d435de09ed5e2af5aee8e39a3f2d12f983d7c5
Waldemar Tomme <waldetomme@gmail.com>
2018-11-15 21:10:25 +0100

    Added Character, Helper

---
+++ Assets/advent-calendar/Scripts/Character.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(CharacterController))]
+public class Character : MonoBehaviour
+{
+    [SerializeField, Range(1f, 5f)]
+    private float movementSpeed = 1f;
+
+    [SerializeField, Range(1f, 5f)]
+    private float rotationSpeed = 1f;
+
+    [SerializeField]
+    private Camera lookCamera;
+
+    private CharacterController controller;
+
+    private void Start()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+
+    private void Update()
+    {
+        UpdatePosition();
+        UpdateRotation();
+    }
+
+    private void UpdatePosition()
+    {
+        var horizontal = Input.GetAxis("Horizontal");
+        var vertical = Input.GetAxis("Vertical");
+
+        var speedVector = new Vector3(horizontal, 0, vertical);
+        speedVector.ClampToOne();
+
+        // TODO speedVector needs to be rotated so it is relative to the gameObject
+        controller.SimpleMove(transform.rotation * speedVector * movementSpeed);
+    }
+
+    private void UpdateRotation()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+
+        var mouseX = Input.GetAxis("Mouse X");
+        var mouseY = Input.GetAxis("Mouse Y");
+
+        transform.Rotate(Vector3.up, mouseX * rotationSpeed, Space.Self);
+        lookCamera.transform.Rotate(Vector3.left, mouseY * rotationSpeed);
+    }
+}
